19f280b5908d5457d8254117df0e6ee2af282730
Feroc1ty <v_ryk@mail.ru>
2021-11-09 07:51:26 +0300

    Коммит 3

--- app/src/main/java/ru/zolotenkov/shopping_list/MainActivity.kt
+++ app/src/main/java/ru/zolotenkov/shopping_list/MainActivity.kt
@@ -1,11 +1,26 @@
 package ru.zolotenkov.shopping_list
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import ru.zolotenkov.shopping_list.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var shopMain: ActivityMainBinding //Все элементы гл. экрана activity_main
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        shopMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(shopMain.root)
+
+        shopMain.button.setOnClickListener {
+            shopMain.textBlock.text = "Комит готов"
+            shopMain.layoutBox.setBackgroundColor(Color.YELLOW)
+        }
+
+
     }
 }
