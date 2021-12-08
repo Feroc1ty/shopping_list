fdebfb465c3e1cf7e45f45d842b32086c7a48d03
Feroc1ty <v_ryk@mail.ru>
2021-12-08 15:25:05 +0300

    Вывод меню

--- app/src/main/java/ru/zolotenkov/shopping_list/MainActivity.kt
+++ app/src/main/java/ru/zolotenkov/shopping_list/MainActivity.kt
@@ -5,11 +5,16 @@ import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View.VISIBLE
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.R
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 import ru.zolotenkov.shopping_list.databinding.ActivityMainBinding
@@ -31,20 +36,27 @@ class MainActivity : AppCompatActivity() {
         myRef.setValue("TextMessage")
 
 
-    }
 
+    }
+    //Блок тулбара
     private fun setUpActionBar(){
         val actionBar = supportActionBar
         Thread{
             val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get() //Получаем картинку профиля с помощью библиотеки Picasso. Get выдает тип Bitmap
             val dIcon = BitmapDrawable(resources, bMap)                     //Превращаем наш битмап в Drawable
-
             runOnUiThread {                                                 //Переводим на основной поток операции с View
                 actionBar?.setDisplayHomeAsUpEnabled(true)                      //Активируем появление кнопки в тулбаре
                 actionBar?.setHomeAsUpIndicator(dIcon)                          //Передаём вместо кнопки назад нашу картинку
                 actionBar?.title = auth.currentUser?.displayName                //Подставляем в тайтл экшнбара имя профиля
+
             }
         }.start()
-
     }
+    //override fun onCreateOptionsMenu(menu: Menu?): Boolean {  ///Функция выполняется при создании тулбара.
+    //    menuInflater.inflate(R.menu.exit, menu)          //Мы передаём в menu нашу разметку main_menu и инфлатор ее раздувает
+    //    return true                                         //Возвращаем тру, ибо функция возвращает Bool
+    //}
+
+
+
 }
