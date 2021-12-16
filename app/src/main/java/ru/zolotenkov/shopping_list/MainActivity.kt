f3a7609480a9fe9322bf4385a28c9991bc890c08
Feroc1ty <v_ryk@mail.ru>
2021-12-16 19:48:14 +0300

    Переделываю на Room. Энтити для бд

--- app/src/main/java/ru/zolotenkov/shopping_list/MainActivity.kt
+++ app/src/main/java/ru/zolotenkov/shopping_list/MainActivity.kt
@@ -29,39 +29,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setUpActionBar()
-
-        myRef = db.getReference(auth.currentUser!!.uid ).child(auth.currentUser?.displayName.toString()).child("products")
-        myRef.setValue("TextMessage")
 
 
 
-    }
-    //Блок тулбара
-    private fun setUpActionBar(){
-        val actionBar = supportActionBar
-        Thread{
-            val bMap = Picasso.get().load(auth.currentUser?.photoUrl).get() //Получаем картинку профиля с помощью библиотеки Picasso. Get выдает тип Bitmap
-            val dIcon = BitmapDrawable(resources, bMap)                     //Превращаем наш битмап в Drawable
-            runOnUiThread {                                                 //Переводим на основной поток операции с View
-                actionBar?.setDisplayHomeAsUpEnabled(true)                      //Активируем появление кнопки в тулбаре
-                actionBar?.setHomeAsUpIndicator(dIcon)                          //Передаём вместо кнопки назад нашу картинку
-                actionBar?.title = auth.currentUser?.displayName                //Подставляем в тайтл экшнбара имя профиля
 
-            }
-        }.start()
-    }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {  ///Функция выполняется при создании тулбара.
-        menuInflater.inflate(R.menu.exit, menu)          //Мы передаём в menu нашу разметку main_menu и инфлатор ее раздувает
-        return true                                         //Возвращаем тру, ибо функция возвращает Bool
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {       //Слушатель нажатий в меню
-        if(item.itemId == R.id.sign_out){                           //Проверка на нажатие
-            auth.signOut()                                          //Выходим из аккаунта
-            finish()                                                //Закрываем активити
-        }
-        return super.onOptionsItemSelected(item)
     }
 
 
--- app/src/main/java/ru/zolotenkov/shopping_list/Product.kt
+++
@@ -1,8 +0,0 @@
-package ru.zolotenkov.shopping_list
-
-data class Product(
-    val userId: Int? = null,
-    val productId: Int? = null,
-    val product: String? = null,
-    val status: Int? = null
-)
