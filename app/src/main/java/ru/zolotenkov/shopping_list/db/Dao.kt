c9a11089af48cff7157d9efabd12babd87b93a21
Feroc1ty <v_ryk@mail.ru>
2021-12-17 13:59:43 +0300

    Создал класс DAO для доступа к БД и воткнул его в функцию которая доступна везде в приложении и через которую будем делать обращения в БД.

---
+++ app/src/main/java/ru/zolotenkov/shopping_list/db/Dao.kt
@@ -0,0 +1,18 @@
+package ru.zolotenkov.shopping_list.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.zolotenkov.shopping_list.entities.NoteItem
+
+// Класс DAO для доступа к базе данных. Тут описываются действия которые мы будем делать в БД, запросы. Для считывания или записи данных.
+
+@Dao
+interface Dao {                                            //Dao должен быть интерфейсом
+    @Query ("SELECT * FROM note_list")                 //Получаем все заметки из бд note_list
+    fun getAllNotes(): Flow<List<NoteItem>>                 //Flow элемент корутин. Этот поток каждый раз берёт данные из БД NoteItem из запроса и запихивает их в список. Список обновляется постоянно при наличии изменений. Нам эту логику прописывать не нужно когда есть Flow.
+
+    @Insert
+    suspend fun insertNote(note: NoteItem)                //обязательно suspend потому что эти функции мы будем запускать внутри корутин. Внутри корутин потому что выполнение этих функций может занять некоторое время.
+}
