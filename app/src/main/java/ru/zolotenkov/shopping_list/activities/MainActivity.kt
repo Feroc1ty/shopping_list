f581be435929c229702c1369e8cc3c3221ddd111
Feroc1ty <v_ryk@mail.ru>
2022-01-24 20:48:54 +0300

    + прикрутил рекламный банер и функцию для его вывода

--- app/src/main/java/ru/zolotenkov/shopping_list/activities/MainActivity.kt
+++ app/src/main/java/ru/zolotenkov/shopping_list/activities/MainActivity.kt
@@ -4,8 +4,15 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.preference.PreferenceManager
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import ru.zolotenkov.shopping_list.R
 import ru.zolotenkov.shopping_list.databinding.ActivityMainBinding
 import ru.zolotenkov.shopping_list.dialogs.NewListDialog
@@ -20,6 +27,9 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
     private var currentMenuItemId = R.id.shop_list
     private lateinit var defPref: SharedPreferences
     private var currentTheme = ""
+    private var iAd: InterstitialAd? = null
+    private var adShowCounter = 0
+    private var adShowCounterMax = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         defPref = PreferenceManager.getDefaultSharedPreferences(this)
@@ -34,17 +44,92 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
              */
         FragmentManager.setFragment(ShopListNamesFragment.newInstance(), this)
         setBottomNavListner()                                           //Кидаем её в цикл активити, слушаем нажатия.
+        loadInterAd()
+    }
+    /*
+    Функция которая загружает рекламу
+    нужно передать контекст, айди рекламы, колбек функции когда загружается реклама
+     */
+    private fun loadInterAd(){
+        val request = AdRequest.Builder().build()
+        InterstitialAd.load(this, getString(R.string.inter_ad_id), request,
+            object : InterstitialAdLoadCallback(){
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    iAd = ad
+                    Log.d("MyLog", "iAd = ad: реклама успешно загрузилась")
+                }
+
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    iAd = null
+                    Log.d("MyLog", "iAd = null: не удалось загрузить рекламу")
+                }
+        })
+    }
+    /*
+    Функция для показа рекламы
+     */
+    private fun showInterAd(adListener: AdListener){
+        if(iAd != null && adShowCounter > adShowCounterMax){
+            iAd?.fullScreenContentCallback = object : FullScreenContentCallback(){
+                /*
+                Выполняется когда закрыли рекламу
+                 */
+                override fun onAdDismissedFullScreenContent() {
+                    iAd = null
+                    loadInterAd()
+                    adListener.onFinish()
+
+                }
+                /*
+                функция выполняется когда произошла какая то ошибка и реклама не показалась
+                 */
+                override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+                    iAd = null
+                    loadInterAd()
+                }
+                /*
+                функция выполняется когда обьявление полностью показалось
+                 */
+                override fun onAdShowedFullScreenContent() {
+                    iAd = null
+                    loadInterAd()
+                }
+            }
+            /*
+            Этой функцией показываем обьявление и сбрасываем счётчик
+             */
+            adShowCounter = 0
+            iAd?.show(this)
+        } else {
+            adShowCounter++
+            Log.d("MyLog", adShowCounter.toString())
+
+            adListener.onFinish()
+        }
     }
 
     private fun setBottomNavListner(){                                  //Функция слушатель нажатий в bottomMenu
         binding.bNav.setOnItemSelectedListener {
             when(it.itemId){
+
+                /*
+                Запускается реклама при переходе в настройки и открывается активити только при выполнении условий для запуска функции onFinish
+                 */
                 R.id.settings ->{
-                    startActivity(Intent(this, SettingsActivity::class.java))
+                    showInterAd(object : AdListener{
+                        override fun onFinish() {
+                            startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
+                        }
+                    })
+
                 }
                 R.id.notes -> {
-                    currentMenuItemId = R.id.notes
-                    FragmentManager.setFragment(NoteFragment.newInstance(), this)
+                    showInterAd(object : AdListener{
+                        override fun onFinish() {
+                            currentMenuItemId = R.id.notes
+                            FragmentManager.setFragment(NoteFragment.newInstance(), this@MainActivity)
+                        }
+                    })
                 }
                 R.id.shop_list -> {
                     currentMenuItemId = R.id.shop_list
@@ -77,6 +162,12 @@ class MainActivity : AppCompatActivity(), NewListDialog.Listener {
     }
     override fun onClick(name: String) {
         Toast.makeText(this, "Its OK", Toast.LENGTH_SHORT).show()
+    }
+
+    interface AdListener{
+        fun onFinish(){
+
+        }
     }
 
 }
